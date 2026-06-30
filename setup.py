d7b3bd5cb627beb810c3431b976b83f070fb13a4
tom <didymusthomas6@gmail.com>
2026-07-01 02:35:06 +0300

    Feat: Add setup to make cleaner builds

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+
+bloom_ext = Extension(
+    name="_cbloom", # the import name
+    sources=[
+        "csrc/bloom.c",
+        "csrc/bloommodule.c",
+    ],
+    include_dirs=["csrc"], # so #include "bloom.h" works
+    extra_compile_args=["-O2", "-std=c99"], # same flags as Makefile
+    libraries=["m"], # link libm for log(), ceil()
+)
+
+setup(
+    ext_modules=[bloom_ext],
+)
