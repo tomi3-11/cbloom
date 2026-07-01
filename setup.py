a55bdac85f317a671193994f895edcabebcc7ba3
tom <didymusthomas6@gmail.com>
2026-07-01 03:52:03 +0300

    Feat: Project cleanup and build

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import Extension, setup
 
 bloom_ext = Extension(
@@ -6,7 +7,7 @@ bloom_ext = Extension(
         "csrc/bloom.c",
         "csrc/bloommodule.c",
     ],
-    include_dirs=["csrc"], # so #include "bloom.h" works
+    include_dirs=["csrc"], # absolute path so #include "bloom.h" works
     extra_compile_args=["-O2", "-std=c99"], # same flags as Makefile
     libraries=["m"], # link libm for log(), ceil()
 )
